ee15f857477719948d42e3999de22eb022d39acd
TeeL18 <torrence_95@hotmail.com>
2024-12-16 18:51:22 +0000

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ function updateWeatherData(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  //console.log(response.data); // it is important to write this as i go along to check in the 'inspect' section to find what responses i need
+
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let mintues = date.getMinutes();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  if (mintues < 10) {
+    mintues = `0${mintues}`;
+  }
+  return `${day}, ${hours}:${mintues}`;
 }
 
 function searchCity(city) {
